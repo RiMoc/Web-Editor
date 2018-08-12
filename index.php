8b82ce3bdd15d0ade0ac27934a7a8a0a9cd588a1
Rimoc <surin.2543.matharin@gmail.com>
2018-08-12 16:58:56 +0700

    .

--- index.php
+++ index.php
@@ -6,8 +6,22 @@
 
     <link rel="stylesheet" type="text/css" href="css/master.css">
     <link rel="icon" href="images/icon.png" sizes="16x16" type="image/png">
+
+    <style media="screen">
+      body{
+        background-image: url("images/bg.jpg");
+        background-repeat: no-repeat;
+        background-position: left top;
+        background-attachment: fixed;
+        background-size: cover;
+
+      }
+    </style>
   </head>
   <body>
+    <div class="title">
+      PN Shop
+    </div>
     <div class="Menu">
         <ul>
           <li>Home Page</li>
