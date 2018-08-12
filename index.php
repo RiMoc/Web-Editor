3d73bcbe42be48105145d006110757cf73141e0b
Rimoc <surin.2543.matharin@gmail.com>
2018-08-12 16:49:07 +0700

    .

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>PN Shop</title>
+
     <link rel="stylesheet" type="text/css" href="css/master.css">
+    <link rel="icon" href="images/icon.png" sizes="16x16" type="image/png">
   </head>
   <body>
     <div class="Menu">
-      <h2>test</h2>
+        <ul>
+          <li>Home Page</li>
+          <li>Products</li>
+          <li>Profile</li>
+        </ul>
     </div>
   </body>
 </html>
